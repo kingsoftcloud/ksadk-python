d2e37ec466233323f3d2297934f4059837e95a25
xiayu <xiayu@kingsoft.com>
2026-06-10 16:42:20 +0800

    chore: move runtime image assets to agentengine-images

--- deploy/hermes/runtime/sitecustomize.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import os
-
-
-if str(os.getenv("HERMES_HOSTED_RUNTIME", "")).strip().lower() not in {"", "0", "false", "no", "off"}:
-    from hosted_gateway import apply_hosted_patches
-
-    apply_hosted_patches()
